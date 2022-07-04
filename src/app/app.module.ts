11114c41a01607dbb97fa378c5d9b92c6a5adc7a
ABBY <abbyW@moringa.com>
2022-07-04 14:19:48 +0300

    set up navbar, footer, sign-up and login components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SignUpComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
