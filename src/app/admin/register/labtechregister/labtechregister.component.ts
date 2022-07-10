f541e6f58120ec464cdead0de5db2c0e92ce8b05
ABBY <abbyW@moringa.com>
2022-07-10 22:18:57 +0300

    Seting up the labtech register component for lab tech sign up.

--- src/app/admin/register/labtechregister/labtechregister.component.ts
+++ src/app/admin/register/labtechregister/labtechregister.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LabregisterService } from 'src/app/services/labregister.service';
 
 @Component({
   selector: 'app-labtechregister',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LabtechregisterComponent implements OnInit {
 
-  constructor() { }
+  name:string =''
+  email:string =''
+  password:string=''
+  confirm_password:string=''
+  username:string= ''
+
+  constructor(private labregisterservice:LabregisterService) { }
 
   ngOnInit(): void {
+  }
+
+  register(): any {
+    if(this.password !== this.confirm_password){
+      alert('Passwords do not match')
+      
+    }
+
+    else if (this.name === '' || this.email === '' || this.username === ''){
+      alert ('Please fill in all the fields')
+    }
+
+    else {
+      this.labregisterservice.registerlabtech({
+        name:this.name, 
+        email: this.email,
+        password:this.password,
+        profile:{
+          username:this.username
+        }
+      }).subscribe(res=>{
+        if (res.status=== 200){
+          alert('Lab Tech Created Successfully')
+        }
+      })
+    }
+
   }
 
 }
